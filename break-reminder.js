d860f4bfb11bc4f9e191d361f5fc94008004e826
ratchek <git@ratchek.com>
2021-12-09 19:01:31 -0500

    Break reminder - initial commit

---
+++ break-reminder.js
@@ -0,0 +1,19 @@
+  /**
+   * Check and set a global guard variable.
+   * If this content script is injected into the same page again,
+   * it will do nothing next time.
+   */
+  // if (window.hasRun) {
+  //   return;
+  // }
+  // window.hasRun = true;
+	//
+function rest_pls() {
+	con = window.confirm("dwa");
+}
+document.body.style.border = "5px solid red";
+
+var timerID = window.setInterval(rest_pls, 1000 * 3)
+	//
+ 	// alert("Hello! I am an alert box!!");
+con = window.confirm("bum tralala la la");
